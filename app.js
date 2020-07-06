cd643885cd0d9d56e5121d9c4fd468b43bb0b494
savimar <sam409@yandex.ru>
2020-07-06 21:42:35 +0300

    fix app.js

--- app.js
+++ app.js
@@ -1,33 +1,68 @@
 const http = require('http');
 const fs = require('fs');
 
-function serve(req, res, type, path, func) {
-    console.log(func);
+function serveStatic(req, res, pathname) {
+
     res.statusCode = 200;
+    let type;
+    let path;
+    if (pathname.endsWith(".css")) {
+        console.log("serveCSS");
+        type = "text/css";
+        path = pathname.slice(1);
+    } else if (pathname.endsWith(".jpeg") || pathname.endsWith(".jpg")) {
+        console.log("serveJPG");
+        type = "image/jpeg";
+        path = pathname.slice(1);
+    } else if (pathname.endsWith(".gif")) {
+        console.log("serveGIF");
+        type = "image/gif";
+        path = pathname.slice(1);
+    } else if (pathname.endsWith(".png")) {
+        console.log("servePNG");
+        type = "image/png";
+        path = pathname.slice(1);
+    } else if (pathname.endsWith(".tiff")) {
+        console.log("serveTIFF");
+        type = "image/tiff";
+        path = pathname.slice(1);
+    } else if (pathname.endsWith("product1")) {
+        console.log("serveProduct1");
+        type = "text/html";
+        path = "html\\product1.html";
+    } else if (pathname.endsWith("product2")) {
+        console.log("serveProduct2");
+        type = "text/html";
+        path = "html\\product2.html";
+    } else {
+
+        type = "text/html";
+        path = "html\\index.html";
+    }
+
     res.setHeader("Content-Type", type);
-    //  let st = fs.createReadStream(__dirname + "\\"+ path, 'utf-8');
-    //  st.pipe(res);
-    let st = fs.readFileSync(path);
-    res.write(st);
+
+    /*if exist file*/
+    if (fs.existsSync(path)) {
+        try {
+            let st = fs.readFileSync(path);
+            res.write(st);
+        } catch (e) {
+            console.log(e);
+            serveNotFound(req, res, 500);
+        }
+    } else {
+        serveNotFound(req, res, 404);
+    }
+
     res.end();
 }
 
-function serveHTML(req, res, path) {
-    serve(req, res, "text/html", path, "serveHTML");
-}
 
-function serveCSS(req, res, path) {
-    serve(req, res, "text/css", path, "serveCSS");
-}
-
-function serveImage(req, res, path) {
-    serve(req, res, "image/jpeg", path, "serveImage");
-}
-
-function serveNotFound(req, res) {
-    let content = "Not found";
+function serveNotFound(req, res, code) {
+    let content = "Not found " + code;
     console.log("serveNotFound");
-    res.writeHead(404, {
+    res.writeHead(code, {
         "Content-Type": "text/html; charset=utf-8"
     });
     res.write(content);
@@ -48,20 +83,20 @@ http.createServer(function (request, response) {
     console.log("Путь", parsedURL.pathname);
 
     if (parsedURL.pathname.startsWith("/img")) {
-        serveImage(request, response, parsedURL.pathname.slice(1));
+        serveStatic(request, response, parsedURL.pathname);
     } else if (parsedURL.pathname.startsWith("/css")) {
-        serveCSS(request, response, parsedURL.pathname.slice(1));
+        serveStatic(request, response, parsedURL.pathname);
     } else {
 
         switch (parsedURL.pathname) {
             case "/":
-                serveHTML(request, response, "html\\index.html");
+                serveStatic(request, response, parsedURL.pathname);
                 break;
             case "/product1":
-                serveHTML(request, response, "html\\product1.html");
+                serveStatic(request, response, parsedURL.pathname);
                 break;
             case "/product2":
-                serveHTML(request, response, "html\\product2.html");
+                serveStatic(request, response, parsedURL.pathname);
                 break;
             default:
                 serveNotFound(request, response);
