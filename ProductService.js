33f61123c4421cc1c88f978c60973c63417685d8
savimar <sam409@yandex.ru>
2020-07-12 14:54:45 +0300

    add HW6.4

--- ProductService.js
+++ ProductService.js
@@ -7,25 +7,39 @@ module.exports = {
       img: "https://i.ibb.co/zfWF2Sw/product1.jpg",
       description: "Краткое описание первого товара",
       descriptionFull: "Полное описание первого товара",
-      price: 1000
+      price: 1000,
+      slug : "bag"
     });
     products.push({
       title: "Товар 2",
       img: "https://i.ibb.co/C7jxQCW/product2.jpg",
       description: "Краткое описание второго товара",
       descriptionFull: "Полное описание второго товара",
-      price: 1500
+      price: 1500,
+      slug : "program"
     });
     products.push({
       title: "Товар 3",
       img: "https://i.ibb.co/Js1JZYs/product3.jpg",
       description: "Краткое описание третьего товара",
       descriptionFull: "Полное описание третьего товара",
-      price: 2000
+      price: 2000,
+      slug : "light"
     });
   },
 
-  getProducts() {
+  getProducts: function () {
     return products;
+  },
+
+  getProductByKey(key, array){
+    if(key>= array.length) {
+      throw new Error("No index in array");
+    }
+    for (let i = 0; i < array.length; i++) {
+      if (i === key) {
+        return array[i];
+      }
+    }
   }
 };
--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const products = ProductService.getProducts();
 
 function serveStatic (req, res, pathname) {
   res.statusCode = 200;
-  let type;
+    let type;
   let path;
   if (pathname.endsWith('.css')) {
     console.log('serveCSS');
@@ -30,12 +30,8 @@ function serveStatic (req, res, pathname) {
     console.log('serveTIFF');
     type = 'image/tiff';
     path = pathname.slice(1);
-  } else if (pathname.endsWith('product')) {
-    console.log('serveProduct1');
-    serveIndex(req, res, 'views\\product.ejs');
-    return;
-  } else {
-    serveIndex(req, res, 'views\\index.ejs');
+    } else {
+   
     return;
   }
   res.setHeader('Content-Type', type);
@@ -56,34 +52,54 @@ function serveStatic (req, res, pathname) {
   res.end();
 }
 
-function serveIndex (req, res, path) {
+function getHTML (pathName, scope, res, req) {
+  try {
+    let htmlStr;
+    ejs.renderFile(pathName, scope, function (err, html) {
+      if (err) {
+        console.log('ERROR: ' + err);
+        return false;
+      }
+      htmlStr = html.toString();
+
+    });
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
+    res.write(htmlStr);
+    res.end();
+  } catch (e) {
+    console.log(e);
+    serveNotFound(req, res, 500);
+  }
+}
+
+function serveIndex (req, res, pathName) {
   console.log('serveIndex');
 
   const scope = {
     products: products
   };
 
+  getHTML(pathName, scope, res, req);
+}
+
+function serveProduct (req, res, path) {
+  console.log('serveIndex');
+
+    let key = path.split("-")[0].replace("/product/", "");
+
   try {
+    const scope = {
+      product: ProductService.getProductByKey(Number.parseInt(key), products)
+    };
+    getHTML('views\\product.ejs', scope, res, req);
 
-   let product;
-     ejs.renderFile(path, scope, function (err, html) {
-     if (err) {
-       console.log("ERROR: " + err);
-       return false;
-     }
-     product = html.toString();
-
-   });
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    });
-    res.write(product);
-    res.end();
   } catch (e) {
-
     console.log(e);
-    serveNotFound(req, res, 500);
   }
+
+
 }
 
 function serveNotFound (req, res, code) {
@@ -99,26 +115,24 @@ function serveNotFound (req, res, code) {
 http.createServer(function (request, response) {
   try {
     console.log('Request, url:', request.url);
-
     const parsedURL = url.parse(request.url, true);
-    console.log('Путь', parsedURL.pathname)
-;
-    if (parsedURL.pathname.startsWith('/img')) {
-      serveStatic(request, response, parsedURL.pathname);
-    } else if (parsedURL.pathname.startsWith('/css')) {
-      serveStatic(request, response, parsedURL.pathname);
+
+let path = parsedURL.pathname;
+if(path.startsWith("/product/css/")){
+  path = path.replace("/product", "");
+}
+
+    console.log('Путь', path);
+    if (path.startsWith('/img')) {
+      serveStatic(request, response, path);
+    } else if (path.startsWith('/css')) {
+      serveStatic(request, response, path);
+    } else if (path.startsWith('/product')) {
+      serveProduct(request, response, path);
+    }else if(path==='/'){
+      serveIndex(request, response, 'views\\index.ejs');
     } else {
-      switch (parsedURL.pathname) {
-        case '/':
-          serveStatic(request, response, parsedURL.pathname);
-          break;
-        case '/product':
-          serveStatic(request, response, parsedURL.pathname);
-          break;
-        default:
-          serveNotFound(request, response, 404);
-          break;
-      }
+     serveNotFound(request, response, 404);
     }
   } catch (e) {
     console.log(e);
